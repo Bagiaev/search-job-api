a80410ed4c03fbee1f83bfb54d091098c0ef06c9
Bagiaev <arsenbagiaev@gmail.com>
2026-02-16 16:14:13 +0300

    test

--- back/internal/models/storage.go
+++ back/internal/models/storage.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type Resume struct {
-	ID        int64     `json:"id"`
-	UserID    int64     `json:"user_id"`
-	Name      string    `json:"name"`
-	City      string    `json:"city"`
-	JobTitle  string    `json:"job_title"`
-	Email     string    `json:"email"`
-	Phone     string    `json:"phone"`
-	CreatedAt time.Time `json:"created_at"`
+	ID         int64     `json:"id"`
+	UserID     int64     `json:"user_id"`
+	Name       string    `json:"name"`
+	Sku        string    `json:"sku"`
+	Barcode    string    `json:"barcode"`
+	CreatedAt  time.Time `json:"created_at"`
+	Updated_at time.Time `json:"updated_at"`
+	Deleted_at time.Time `json:"deleted_at"`
 }
